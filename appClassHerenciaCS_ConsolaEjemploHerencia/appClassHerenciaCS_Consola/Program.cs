6f06b35bbc90ba676e2f38d0600b007c32b9afcb
OscarTorres37 <89743953+OscarTorres37@users.noreply.github.com>
2021-10-12 08:54:34 -0500

    Add files via upload

---
+++ appClassHerenciaCS_ConsolaEjemploHerencia/appClassHerenciaCS_Consola/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClassHerenciaCS_Consola
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Punto2D a;
+      a = new Punto2D(98,76);
+      Console.WriteLine(a.ToString());
+
+      Punto a2;
+      a2 = new Punto(5.99);
+
+      Console.WriteLine(a2.ToString());
+
+            Punto3D b;
+            b = new Punto3D(99, 78);
+            Console.WriteLine(b.ToString());
+
+            Punto b2;
+            b2 = new Punto(7.89);
+            Console.WriteLine(b2.ToString());
+
+
+
+      Console.ReadKey();
+
+
+
+    }
+  }
+}
---
+++ appClassHerenciaCS_ConsolaEjemploHerencia/appClassHerenciaCS_Consola/Punto3D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClassHerenciaCS_Consola
+{
+    class Punto3D : Punto2D
+    {
+        private double z;
+
+        public double M
+        {
+            get { return z; }
+            set { z = value; }
+        }
+        public Punto3D() : base()
+        {
+
+            z = 0;
+
+        }
+
+        public Punto3D(double y, double z) : base()
+        {
+
+            this.z = z;
+
+        }
+
+        public double Distancia(Punto3D p)
+        {
+            double d = 0;
+            d = z - p.z; // Valor Absoluto
+            return d;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " Z=" + z;
+        }
+    }
+}
